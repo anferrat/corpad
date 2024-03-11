7f988f960193e8ed0d7b95c7e2a8ae15037e582f
Andrei <andrei.lomtev@gmail.com>
2024-03-10 23:11:02 -0700

    Added delay for splash screen to disappear on start

--- src/features/navigation/components/SplashScreen.js
+++ src/features/navigation/components/SplashScreen.js
@@ -1,16 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { StyleSheet, View } from "react-native"
 import { primary } from "../../../styles/colors"
 import FocusAwareStatusBar from "../../../components/FocusAwareStatusBar"
 import WaveActivityIndicator from "../../../components/WaveActivityIndicator"
 
-const SplashScreen = () => {
-    return (
-        <View style={styles.splash}>
-            <FocusAwareStatusBar translucent={true} barStyle='light-content' backgroundColor={'transparent'} />
-            <WaveActivityIndicator size='large' color='#fff' />
-        </View>
-    )
+const SplashScreen = ({ loading }) => {
+    const [visible, setVisible] = useState(loading)
+    useEffect(() => {
+        //Give time for stack navigator and all the displayed screen to render
+        if (!loading)
+            setTimeout(() => setVisible(false), 400)
+    },
+        [loading])
+    if (visible)
+        return (
+            <View style={styles.splash}>
+                <FocusAwareStatusBar translucent={true} barStyle='light-content' backgroundColor={'transparent'} />
+                <WaveActivityIndicator size='large' color='#fff' />
+            </View>
+        )
 }
 
 export default SplashScreen
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -42,73 +42,75 @@ const Stack = createNativeStackNavigator()
 
 export const AppNavigator = () => {
   const { loading, isCloud, isLoaded, isOnboardingVisible } = useApp()
-  if (loading)
-    return <SplashScreen />
-  else
-    return (
-      <>
-        <MultimeterModal />
-        <Paywall />
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: true,
-            animation: 'fade',
-            //insets has to be passed to header from the top, to avoid glitching when swicthing between screens
-            header: ({ route, navigation, options }) => <TopBar screen={route.name} params={route.params} navigation={navigation} topInset={options.headerStatusBarHeight} />,
-          }}>
-          {isOnboardingVisible ? <Stack.Screen name='Onboarding' component={OnboardingScreen} /> : null}
-          {
-            isLoaded ? (
-              <>
-                <Stack.Screen name='PipelineSurvey' component={SurveyBottomTabs} options={{ headerShown: false }} />
-                <Stack.Screen name='ViewItem' component={ViewItem} />
-                <Stack.Screen name='EditMapLayer' component={EditMapLayer} />
-                <Stack.Screen name='ViewMapLayer' component={ViewMapLayer} />
-                <Stack.Screen name='ViewMarkerInfo' component={ViewMarkerInfo} />
-                <Stack.Screen name='EditItem' component={EditItem} />
-                <Stack.Screen name='EditSubitem' component={EditSubitem} />
-                <Stack.Group screenOptions={{
-                  animation: 'fade_from_bottom',
-                }}>
-                  <Stack.Screen name='ImportItem' component={ImportItem} />
-                  <Stack.Screen name='ImportSubitem' component={ImportSubitem} />
-                  <Stack.Screen name='ImportFile' component={ImportFile} />
-                  <Stack.Screen name='ImportParameters' component={ImportParameters} />
-                  <Stack.Screen name='Settings' component={SettingsScreen} />
-                  <Stack.Screen name='ExportItem' component={ExportItem} />
-                  <Stack.Screen name='ExportPotentials' component={ExportPotentials} />
-                  <Stack.Screen name='ExportSubitems' component={ExportSubitems} />
-                  <Stack.Screen name='ExportOverview' component={ExportOverview} />
-                </Stack.Group >
-                <Stack.Group screenOptions={{ animation: 'fade' }}>
-                  <Stack.Screen name='Search' component={SearchBar} />
-                </Stack.Group>
-              </>
-            ) : (
-              <>
-                <Stack.Screen name='Home' component={HomeBottomTabs} initialParams={{ homeScreenCloud: isCloud }} />
-                <Stack.Group screenOptions={{ animation: 'fade' }}>
-                  <Stack.Screen name='CreateSurvey' component={CreateSurvey} initialParams={{ withImport: false }} />
-                </Stack.Group>
-              </>
-            )
-          }
-          <Stack.Group screenOptions={{ animation: 'fade' }}>
-            <Stack.Screen name='Spreadsheet' component={Spreadsheet} initialParams={{ uri: null, title: null }} />
-            <Stack.Screen name='ExternalLink' component={ExternalLink} initialParams={{ link: null }} />
-            <Stack.Screen name='FindItemInSurvey' component={FindItem} />
-            <Stack.Screen name='PipelineMatching' component={PipelineMatching} />
-          </Stack.Group>
-          <Stack.Group screenOptions={{ animation: 'fade_from_bottom' }}>
-            <Stack.Screen name='CalculatorList' component={CalculatorList} />
-            <Stack.Screen name='Calculator' component={Calculator} />
-            <Stack.Screen name='DevScreen' component={DevScreen} />
-            <Stack.Screen name='CalculatorDescription' component={CalculatorDescription} />
-            <Stack.Screen name='SettingDetails' component={SettingDetails} />
-            <Stack.Screen name='Licenses' component={Licenses} />
-            <Stack.Screen name='CycleSettings' component={CycleSettings} />
-          </Stack.Group>
-        </Stack.Navigator >
-      </>
-    )
+  return (
+    <>
+      {!loading ?
+        <>
+          <MultimeterModal />
+          <Paywall />
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: true,
+              animation: 'fade',
+              //insets has to be passed to header from the top, to avoid glitching when swicthing between screens
+              header: ({ route, navigation, options }) => <TopBar screen={route.name} params={route.params} navigation={navigation} topInset={options.headerStatusBarHeight} />,
+            }}>
+            {isOnboardingVisible ? <Stack.Screen name='Onboarding' component={OnboardingScreen} /> : null}
+            {
+              isLoaded ? (
+                <>
+                  <Stack.Screen name='PipelineSurvey' component={SurveyBottomTabs} options={{ headerShown: false }} />
+                  <Stack.Screen name='ViewItem' component={ViewItem} />
+                  <Stack.Screen name='EditMapLayer' component={EditMapLayer} />
+                  <Stack.Screen name='ViewMapLayer' component={ViewMapLayer} />
+                  <Stack.Screen name='ViewMarkerInfo' component={ViewMarkerInfo} />
+                  <Stack.Screen name='EditItem' component={EditItem} />
+                  <Stack.Screen name='EditSubitem' component={EditSubitem} />
+                  <Stack.Group screenOptions={{
+                    animation: 'fade_from_bottom',
+                  }}>
+                    <Stack.Screen name='ImportItem' component={ImportItem} />
+                    <Stack.Screen name='ImportSubitem' component={ImportSubitem} />
+                    <Stack.Screen name='ImportFile' component={ImportFile} />
+                    <Stack.Screen name='ImportParameters' component={ImportParameters} />
+                    <Stack.Screen name='Settings' component={SettingsScreen} />
+                    <Stack.Screen name='ExportItem' component={ExportItem} />
+                    <Stack.Screen name='ExportPotentials' component={ExportPotentials} />
+                    <Stack.Screen name='ExportSubitems' component={ExportSubitems} />
+                    <Stack.Screen name='ExportOverview' component={ExportOverview} />
+                  </Stack.Group >
+                  <Stack.Group screenOptions={{ animation: 'fade' }}>
+                    <Stack.Screen name='Search' component={SearchBar} />
+                  </Stack.Group>
+                </>
+              ) : (
+                <>
+                  <Stack.Screen name='Home' component={HomeBottomTabs} initialParams={{ homeScreenCloud: isCloud }} />
+                  <Stack.Group screenOptions={{ animation: 'fade' }}>
+                    <Stack.Screen name='CreateSurvey' component={CreateSurvey} initialParams={{ withImport: false }} />
+                  </Stack.Group>
+                </>
+              )
+            }
+            <Stack.Group screenOptions={{ animation: 'fade' }}>
+              <Stack.Screen name='Spreadsheet' component={Spreadsheet} initialParams={{ uri: null, title: null }} />
+              <Stack.Screen name='ExternalLink' component={ExternalLink} initialParams={{ link: null }} />
+              <Stack.Screen name='FindItemInSurvey' component={FindItem} />
+              <Stack.Screen name='PipelineMatching' component={PipelineMatching} />
+            </Stack.Group>
+            <Stack.Group screenOptions={{ animation: 'fade_from_bottom' }}>
+              <Stack.Screen name='CalculatorList' component={CalculatorList} />
+              <Stack.Screen name='Calculator' component={Calculator} />
+              <Stack.Screen name='DevScreen' component={DevScreen} />
+              <Stack.Screen name='CalculatorDescription' component={CalculatorDescription} />
+              <Stack.Screen name='SettingDetails' component={SettingDetails} />
+              <Stack.Screen name='Licenses' component={Licenses} />
+              <Stack.Screen name='CycleSettings' component={CycleSettings} />
+            </Stack.Group>
+          </Stack.Navigator >
+        </> : null}
+      <SplashScreen
+        loading={loading} />
+    </>
+  )
 }
