5fb9b4c138922136a6d611dc5999eefdc68f7427
Andrei Lomtev (Mac) <andrei.lomtev@gmail.com>
2024-03-12 18:49:12 -0700

    Ios and app version is bumped to 1.5

--- App.tsx
+++ App.tsx
@@ -54,7 +54,7 @@ export const ScrollRef = createContext<MutableRefObject<any> | null>(null);
 
 ModalService.setShouldUseTopInsets = true;
 
-export const version = '1.4';
+export const version = '1.5';
 export const DEVELOPER_MODE_ON = true;
 
 function App(): JSX.Element {
