0918f1b2c7dc0a964f6b513dc4e87e6d30b12972
Andrei Lomtev <andrei.lomtev@gmail.com>
2024-07-17 23:06:10 -0700

    Changed ios privacy, added scripts

--- .cxx/local/armeabi-v7a/objs-debug/event/strlcpy.o.d
+++
@@ -1,8 +0,0 @@
-C:\ReactNative\Corpad\.cxx/local/armeabi-v7a/objs-debug/event/strlcpy.o: \
-  C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\strlcpy.c \
-  C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\include\event2\event-config.h \
-  C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\evconfig-private.h
-
-C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\include\event2\event-config.h:
-
-C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\evconfig-private.h:
--- .cxx/local/x86/objs-debug/event/strlcpy.o.d
+++
@@ -1,8 +0,0 @@
-C:\ReactNative\Corpad\.cxx/local/x86/objs-debug/event/strlcpy.o: \
-  C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\strlcpy.c \
-  C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\include\event2\event-config.h \
-  C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\evconfig-private.h
-
-C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\include\event2\event-config.h:
-
-C:\ReactNative\Corpad\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\evconfig-private.h:
