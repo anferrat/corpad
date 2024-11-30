49a90e2701b087d879f982c5bc8f941dc576e40e
Andrei Lomtev <andrei.lomtev@gmail.com>
2024-11-29 16:35:58 -0800

    updated toKml lib

--- src/app/services/survey/manager/export/csv/_export_to_spreadsheet/_GetFileConetnt.js
+++ src/app/services/survey/manager/export/csv/_export_to_spreadsheet/_GetFileConetnt.js
@@ -1,4 +1,5 @@
 import { ExportFormatTypes } from "../../../../../../../constants/global";
+import { MapLayerFillColors, MapLayerStrokeColors } from "../../../../../../../styles/colors";
 
 export class _GetFileContent {
     constructor(mapLayerRepo, csvParser, parseToKml) {
@@ -19,10 +20,42 @@ export class _GetFileContent {
         }
     }
 
+    _getStyleProperties(type, color, strokeWidth) {
+        //Only stroke color is used as a general color property for everything. Polygons filled with stroke color and 0.5 opacity.
+        switch (type) {
+            case 'Point':
+                return {
+                    "marker-color": MapLayerStrokeColors[color],
+                }
+            case 'LineString':
+            case 'MultiLineString':
+            case 'Polygon':
+            case 'MultiPolygon':
+                return {
+                    "stroke": MapLayerStrokeColors[color],
+                    "stroke-width": Number(strokeWidth),
+                    "stroke-opacity": 1.0,
+                    "fill": MapLayerStrokeColors[color],
+                    "fill-opacity": 0.5
+                }
+            default:
+                return {}
+        }
+    }
+
     async _getMapLayerFeatures() {
         const mapLayers = await this.mapLayerRepo.getAll()
-        return mapLayers.filter(({ visible }) => visible).map(({ data }) => {
-            return this._convertData(data).features
+        return mapLayers.filter(({ visible }) => visible).map(({ data, strokeColor, strokeWidth }) => {
+            return this._convertData(data).features.map(feature => {
+                const styleProps = this._getStyleProperties(feature.geometry?.type, strokeColor, strokeWidth)
+                return ({
+                    ...feature,
+                    properties: {
+                        ...feature.properties,
+                        ...styleProps,
+                    }
+                })
+            })
         }).flat(1)
     }
 
