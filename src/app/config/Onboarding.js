85ef19379aed5fe256eb2f6f23e9a4c9b7923c39
Andrei Lomtev <andrei.lomtev@gmail.com>
2025-07-19 10:31:20 -0700

    Updated version for ios, android and app

--- src/app/config/Onboarding.js
+++ src/app/config/Onboarding.js
@@ -1,4 +1,4 @@
-export const ONBOARDING_VERSION = 7
+export const ONBOARDING_VERSION = 8
 /*
 To create new onboarding on update increase ONBOARDING_VERSION by one, and update RN components for Onboarding screen.
 */
